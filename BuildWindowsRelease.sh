352e68647730f185bac297ca54fbd1c21d4ef760
adrpo <adrpo@f25d12d1-65f4-0310-ae8a-bbce733d8d8e>
2012-10-08 17:18:51 +0000

    - include the testing hints and some more info.
    git-svn-id: https://openmodelica.org/svn/OpenModelica/installers/windows/OpenModelicaSetup@13262 f25d12d1-65f4-0310-ae8a-bbce733d8d8e

--- BuildWindowsRelease.sh
+++ BuildWindowsRelease.sh
@@ -70,14 +70,45 @@ svn log -v -r ${REVISION}:1 > ${FILE_PREFIX}-ChangeLog.txt
 
 # make the readme
 export DATESTR=`date +"%Y-%m-%d_%H-%M"`
-echo "Automatic build by testwin.openmodelica.org in date: ${DATESTR} from revision: ${REVISION}" >> ${FILE_PREFIX}-README.txt
+echo "Automatic build of OpenModelica by testwin.openmodelica.org at date: ${DATESTR} from revision: ${REVISION}" >> ${FILE_PREFIX}-README.txt
 echo "Read OpenModelica-*-ChangeLog.txt for more info on changes." >> ${FILE_PREFIX}-README.txt
 echo "See also (match revision ${REVISION} to build jobs):" >> ${FILE_PREFIX}-README.txt
 echo "  https://test.openmodelica.org/hudson/" >> ${FILE_PREFIX}-README.txt
 echo "  http://test.openmodelica.org/~marsj/MSL31/BuildModelRecursive.html" >> ${FILE_PREFIX}-README.txt
 echo "  http://test.openmodelica.org/~marsj/MSL32/BuildModelRecursive.html" >> ${FILE_PREFIX}-README.txt
+cat >> ${FILE_PREFIX}-README.txt <<DELIMITER
+*Instructions to prepare test information if you find a bug:*
+ 
+generate a .mos script file loading all libraries and files your model need call simulate.
+// start .mos script
+loadModel(Modelica);
+loadFile("yourfile.mo");
+simulate(YourModel);
+// end .mos script
+
+Start this .mos script in a shell with omc and use the debug flags
++d=dumpdaelow,optdaedump,bltdump,dumpindxdae,backenddaeinfo.
+Redirect the output stream in file ( > log.txt)
+
+A series of commands to run via cmd.exe
+is given below. Note that z: is the drive
+where your .mos script is:
+c:\> z:
+z:\> cd \path\to\script(.mos)\file\
+z:\path\to\script(.mos)\file\> \path\to\OpenModelica\bin\omc.exe
++d=dumpdaelow,optdaedump,bltdump,dumpindxdae,backenddaeinfo 
+YourScriptFile.mos > log.txt 2>&1
+
+Either send the log.txt file alongwith your bug 
+description to OpenModelica@ida.liu.se or file a
+bug in our bug tracker:
+  https://trac.openmodelica.org/OpenModelica
+
+Happy testing!
+DELIMITER
 echo " " >> ${FILE_PREFIX}-README.txt
-echo "Contact OpenModelica@ida.liu.se for questions." >> ${FILE_PREFIX}-README.txt
+echo "Read more about OpenModelica at https://openmodelica.org" >> ${FILE_PREFIX}-README.txt
+echo "Contact us at OpenModelica@ida.liu.se for further issues or questions." >> ${FILE_PREFIX}-README.txt
 
 # make the testsuite-trace
 echo "Check HUDSON testserver for the testsuite trace here (match revision ${REVISION} to build jobs): " >> ${FILE_PREFIX}-testsuite-trace.txt
