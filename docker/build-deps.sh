dbb1eb55e69051fba9a4015ccdc0e96d619d9abc
Martin Sjölund <martin.sjolund@liu.se>
2018-06-08 15:24:50 +0200

    More build dependencies

--- docker/build-deps.sh
+++ docker/build-deps.sh
@@ -1,8 +1,7 @@
 #!/bin/sh -xe
 
 sh -c 'sudo docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password`'
-cp ../debian/control .
-if true; then
+if false; then
   ARGS="--build-arg REPO=ubuntu --build-arg DISTRO=bionic"
 fi
 sudo docker build $ARGS -t openmodelica/build-deps:latest - < Dockerfile.build-deps
