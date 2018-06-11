6219f7dddcecf346fb86eadc56ee1de63cc70150
Martin Sjölund <martin.sjolund@liu.se>
2018-06-11 09:46:03 +0200

    Make a slim moparser

--- docker/moparser.sh
+++ docker/moparser.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -xe
 
-sh -c 'sudo docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password`'
-sudo docker build -t openmodelica/moparser - < Dockerfile.moparser
-sudo docker push openmodelica/moparser
+cd moparser
+sh -c 'docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password`'
+wget https://trac.modelica.org/Modelica/export/9902/Modelica/branches/tools/MoParser/Linux64/moparser -O moparser
+chmod +x moparser
+docker build -t openmodelica/moparser:3.4 .
+docker tag openmodelica/moparser:3.4 openmodelica/moparser:latest
+docker push openmodelica/moparser:latest
+docker push openmodelica/moparser:3.4
