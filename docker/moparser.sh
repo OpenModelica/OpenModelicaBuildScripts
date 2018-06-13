7f0b64d89eb1b2a55d6c9828a9aae7cbdbea580d
Martin Sjölund <martin+github@sjoelund.se>
2018-06-13 13:08:08 +0200

    Link to specific hash

--- docker/moparser.sh
+++ docker/moparser.sh
@@ -2,7 +2,7 @@
 
 cd moparser
 sh -c 'docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password`'
-wget https://github.com/modelica-tools/ModelicaSyntaxChecker/raw/master/Linux64/moparser -O moparser
+wget https://github.com/modelica-tools/ModelicaSyntaxChecker/raw/e759da7cdea44391ec84c663275f03a6771381cc/Linux64/moparser -O moparser
 chmod +x moparser
 docker build -t openmodelica/moparser:3.4 .
 docker tag openmodelica/moparser:3.4 openmodelica/moparser:latest
