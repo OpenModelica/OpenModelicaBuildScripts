cb24278a72f536dd4745d2867843c87afaa48425
Martin Sjölund <martin.sjolund@liu.se>
2018-06-14 09:59:28 +0200

    zip is needed for OMSimulator

--- docker/build-deps-32.sh
+++ docker/build-deps-32.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -xe
 
-sh -c 'sudo docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password`'
+docker login docker.openmodelica.org
 if false; then
   ARGS="--build-arg REPO=i386/ubuntu --build-arg DISTRO=bionic"
 fi
 
 TAG=docker.openmodelica.org/build-deps:v1.13-i386
-sudo docker build $ARGS -t "$TAG" - < Dockerfile.build-deps-i386
-sudo docker push "$TAG"
+docker build $ARGS -t "$TAG" - < Dockerfile.build-deps-i386
+docker push "$TAG"
