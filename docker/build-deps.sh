672340c40bb795a45a2f2eaf40008b8002018224
Martin Sjölund <martin.sjolund@liu.se>
2018-06-14 09:07:31 +0200

    Add 32-bit build deps

--- docker/build-deps.sh
+++ docker/build-deps.sh
@@ -4,5 +4,10 @@ sh -c 'sudo docker login -u openmodelica -p=`cat ~/.docker/openmodelica.password
 if false; then
   ARGS="--build-arg REPO=ubuntu --build-arg DISTRO=bionic"
 fi
-sudo docker build $ARGS -t openmodelica/build-deps:latest - < Dockerfile.build-deps
-sudo docker push openmodelica/build-deps:latest
+
+LATEST=docker.openmodelica.org/build-deps:latest
+TAG=docker.openmodelica.org/build-deps:v1.13
+docker build $ARGS -t "$TAG" - < Dockerfile.build-deps
+docker tag "$TAG" "$LATEST"
+docker push "$TAG"
+docker push "$LATEST"
