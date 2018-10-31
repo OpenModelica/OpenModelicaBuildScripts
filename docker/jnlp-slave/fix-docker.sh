bc9f9fa116f68467226242c4637fcbe92bc5136e
Martin Sjölund <martin.sjolund@liu.se>
2018-10-31 09:27:48 +0100

    Update rules to build in parallel

--- docker/jnlp-slave/fix-docker.sh
+++ docker/jnlp-slave/fix-docker.sh
@@ -4,8 +4,13 @@ if ! test -e /var/run/docker.sock; then
   echo "/var/run/docker.sock does not exist"
   exit 1
 fi
-addgroup --gid "`stat -c '%g' /var/run/docker.sock`" dockersock
-usermod -a -G dockersock jenkins
+GROUP="`stat -c '%g' /var/run/docker.sock`"
+if test "UNKNOWN" = "$GROUP"; then
+  addgroup --gid "`stat -c '%g' /var/run/docker.sock`" dockersock
+  usermod -a -G dockersock jenkins
+else
+  usermod -a -G "$GROUP" jenkins
+fi
 touch /tmp/dockersock.test
 chmod 440 /tmp/dockersock.test
 chown root:dockersock /tmp/dockersock.test
