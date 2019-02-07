7640466ea8b082a62bf09b9650f16c9821298e97
Martin Sjölund <martin.sjolund@liu.se>
2019-02-07 21:31:25 +0100

    Add pacman to the wine-rtest image

--- docker/wine-rtest.sh
+++ docker/wine-rtest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
-docker login docker.openmodelica.org
+# docker login docker.openmodelica.org
 
 TAG=docker.openmodelica.org/wine-rtest:v2.0
 docker build --pull -t "$TAG" wine-rtest
