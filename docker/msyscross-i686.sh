233e8db4422ebb233e40c8498a080c81d204733c
Martin Sjölund <martin.sjolund@liu.se>
2019-02-13 09:20:23 +0100

    Separate the msys cross-compilation into two images

---
+++ docker/msyscross-i686.sh
@@ -0,0 +1,1 @@
+sudo docker build --build-arg DIST=ubuntu:bionic --build-arg LINKCPPFS=false --build-arg ARCH=i686 -t docker.openmodelica.org/msyscross-omsimulator:v2.1.0-i686 msyscross
---
+++ docker/msyscross.sh
@@ -0,0 +1,1 @@
+sudo docker build -t docker.openmodelica.org/msyscross-omsimulator:v2.1.0 msyscross
