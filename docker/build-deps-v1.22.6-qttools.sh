d102dfb8886ed5ee8184b417ab8e547c2bcfbaac
Adrian Pop <adrian.pop@liu.se>
2024-12-16 20:01:49 +0100

    is getting ridiculous

--- docker/build-deps-v1.22.6-qttools.sh
+++ docker/build-deps-v1.22.6-qttools.sh
@@ -1,6 +1,6 @@
-export TAG=v1.22.5-qttools
+export TAG=v1.22.6-qttools
 export REGISTRY=docker.openmodelica.org
-docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.build-deps-v1.22.5-qttools
+docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.$TAG
 docker login
 docker image tag build-deps:$TAG $REGISTRY/build-deps:$TAG
 docker push $REGISTRY/build-deps:$TAG
