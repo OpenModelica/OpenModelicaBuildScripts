636b1e002af261be48f8642b382c109252dea050
Adrian Pop <adrian.pop@liu.se>
2024-12-16 20:07:49 +0100

    proper prefix

--- docker/build-deps-v1.22.6-qttools.sh
+++ docker/build-deps-v1.22.6-qttools.sh
@@ -1,6 +1,6 @@
 export TAG=v1.22.6-qttools
 export REGISTRY=docker.openmodelica.org
-docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.$TAG
+docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.build-deps-$TAG
 docker login
 docker image tag build-deps:$TAG $REGISTRY/build-deps:$TAG
 docker push $REGISTRY/build-deps:$TAG
