07682313a8064494edd46d7481a6a7c63cb0b410
Adrian Pop <adrian.pop@liu.se>
2024-12-16 19:17:52 +0100

    more packages needed

--- docker/build-deps-v1.22.5-qttools.sh
+++ docker/build-deps-v1.22.5-qttools.sh
@@ -1,6 +1,6 @@
-export TAG=v1.22.4-qttools
+export TAG=v1.22.5-qttools
 export REGISTRY=docker.openmodelica.org
-docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.build-deps-v1.22.4-qttools
+docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.build-deps-v1.22.5-qttools
 docker login
 docker image tag build-deps:$TAG $REGISTRY/build-deps:$TAG
 docker push $REGISTRY/build-deps:$TAG
