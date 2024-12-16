c3f979370a3297e34e209f58ac5726f1987fdd9e
Adrian Pop <adrian.pop@liu.se>
2024-12-16 13:25:31 +0100

    adapt build-deps-v1.22.4-qttools.sh

--- docker/build-deps-v1.22.4-qttools.sh
+++ docker/build-deps-v1.22.4-qttools.sh
@@ -1,6 +1,6 @@
 export TAG=v1.22.4-qttools
 export REGISTRY=docker.openmodelica.org
-docker build --pull --no-cache --tag build-deps:$TAG .
+docker build --pull --no-cache --tag build-deps:$TAG - < Dockerfile.build-deps-v1.22.4-qttools
 docker login
 docker image tag build-deps:$TAG $REGISTRY/build-deps:$TAG
 docker push $REGISTRY/build-deps:$TAG
